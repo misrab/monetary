0a5db0387e424bc014f33c36accbbd184a7ff619
misrab <faizullah.misrab@gmail.com>
2017-07-13 20:00:17 +0700

    commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import future
 
 from price_feed import *
-
+from bank import *
 
 
 
 if __name__ == '__main__':
     print("Running main...")
+
+    feed = Feed()
+    bank = Bank(feed)
+    users = [ User() for i in xrange(10) ]
